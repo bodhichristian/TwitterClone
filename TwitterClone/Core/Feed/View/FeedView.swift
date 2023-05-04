868fb68cfd9c5fe34d9f1950baa055cf9e9bc17c
bodhichristian <110639779+bodhichristian@users.noreply.github.com>
2023-05-04 16:43:30 -0400

    Refactored, commented and cleaned up FeedView.swift

--- TwitterClone/Core/Feed/View/FeedView.swift
+++ TwitterClone/Core/Feed/View/FeedView.swift
@@ -12,58 +12,15 @@ struct FeedView: View {
     @EnvironmentObject var authVM: AuthViewModel
     @ObservedObject var feedVM = FeedViewModel()
     
-    @Namespace var animation // For animating blue bar in tweetFilter
-    @State private var selectedFeed: FeedType = .forYou
     @Binding var showingSideMenu: Bool
+    @State private var selectedFeed: FeedType = .forYou
+    @Namespace var animation // For animating blue bar in tweetFilter
     
     var body: some View {
         
         VStack {
-            HStack {
-                Button {
-                    withAnimation{
-                        showingSideMenu = true
-                    }
-                } label: {
-                    if let profilePhoto = authVM.currentUser?.profilePhotoUrl {
-                        KFImage(URL(string: profilePhoto))
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 32, height: 32)
-                            .clipShape(Circle())
-                           // .padding(.leading, 5)
-                        
-                    } else {
-                        Circle()
-                            .frame(width: 32)
-                            .foregroundColor(.twitterBlue)
-                            //.padding(.leading, -4)
-                    }
-                }
-
-                
-                Spacer()
-                
-                TwitterLogo()
-                   // .padding(8)
-                    .opacity(showingSideMenu ? 0 : 1)
-                
-                Spacer()
-                
-                Circle()
-                    .frame(width: 32)
-                    .foregroundColor(.clear)
-            }
-            .padding(.horizontal)
             header
-            // Tweets
-            ScrollView {
-                LazyVStack {
-                    ForEach(0..<20, id: \.self) { _ in
-                        TweetRowView()
-                    }
-                }
-            }
+            feed
         }
     }
 }
@@ -83,35 +40,89 @@ extension FeedView {
                 .frame(height: 144)
                 .offset(y: -100)
                 .padding(.bottom, -100)
-            // For You, Following
-            HStack {
-                ForEach(FeedType.allCases, id: \.rawValue) { feedType in
-                    VStack {
-                        Text(feedType.title)
-                            .font(.caption)
-                            .fontWeight(.semibold)
-                            .foregroundColor(selectedFeed == feedType ? .primary : .secondary)
-                            .padding(.horizontal, 66)
-                        if selectedFeed == feedType {
-                            Capsule()
-                                .foregroundColor(Color.twitterBlue)
-                                .frame(width: 62, height: 3)
-                                .matchedGeometryEffect(id: "filter", in: animation)
+            
+            VStack {
+                // Header top row
+                HStack {
+                    // Profile Photo
+                    Button {
+                        // Display SideMenuView
+                        withAnimation{
+                            showingSideMenu = true
+                        }
+                    } label: {
+                        // If current user has a profile image
+                        if let profilePhoto = authVM.currentUser?.profilePhotoUrl {
+                            // Fetch image
+                            KFImage(URL(string: profilePhoto))
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 32, height: 32)
+                                .clipShape(Circle())
+                            
                         } else {
-                            Capsule()
-                                .foregroundColor(.clear)
-                                .frame(width: 62, height: 3)
+                            // Placeholder
+                            Circle()
+                                .frame(width: 32)
+                                .foregroundColor(.twitterBlue)
                         }
                     }
-                    .onTapGesture {
-                        withAnimation(.easeInOut(duration: 0.2)) {
-                            self.selectedFeed = feedType
+                    
+                    Spacer()
+                    
+                    TwitterLogo()
+                        .opacity(showingSideMenu ? 0 : 1)
+                    
+                    Spacer()
+                    
+                    // Placeholder to match header in ExploreView
+                    Circle()
+                        .frame(width: 32)
+                        .foregroundColor(.clear)
+                }
+                .padding(.horizontal)
+                
+                // For You, Following
+                HStack {
+                    ForEach(FeedType.allCases, id: \.rawValue) { feedType in
+                        VStack {
+                            Text(feedType.title)
+                                .font(.caption)
+                                .fontWeight(.semibold)
+                                .foregroundColor(selectedFeed == feedType ? .primary : .secondary)
+                                .padding(.horizontal, 66)
+                            if selectedFeed == feedType {
+                                Capsule()
+                                    .foregroundColor(Color.twitterBlue)
+                                    .frame(width: 62, height: 3)
+                                    .matchedGeometryEffect(id: "filter", in: animation)
+                            } else {
+                                Capsule()
+                                    .foregroundColor(.clear)
+                                    .frame(width: 62, height: 3)
+                            }
+                        }
+                        .onTapGesture {
+                            withAnimation(.easeInOut(duration: 0.2)) {
+                                self.selectedFeed = feedType
+                            }
                         }
                     }
                 }
+                .overlay(Divider().offset(y: 14))
+                .offset(y: 8)
             }
-            .overlay(Divider().offset(y: 14))
-            .offset(y: 8)
+        }
+    }
+    
+    var feed: some View {
+        // Tweets
+        ScrollView {
+            LazyVStack {
+                ForEach(0..<20, id: \.self) { _ in
+                    TweetRowView()
+                }
+            }
         }
     }
 }
