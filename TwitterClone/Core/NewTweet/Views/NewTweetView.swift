f00b920a7245144adf0d16d781d984ca72f6d218
bodhichristian <110639779+bodhichristian@users.noreply.github.com>
2023-05-18 11:38:15 -0400

    Upgraded NewTweetView to show all currently available tweet buttons. User can now add an image to a tweet, though upload functionality is not yet in place.

--- TwitterClone/Core/NewTweet/Views/NewTweetView.swift
+++ TwitterClone/Core/NewTweet/Views/NewTweetView.swift
@@ -15,70 +15,27 @@ struct NewTweetView: View {
     @ObservedObject var newTweetVM = UploadTweetViewModel()
     
     @State private var tweetBody = ""
+    @State private var showingImagePicker = false
+    @State private var selectedImage: UIImage?
+    
+    @FocusState private var focusTweetBody: Bool
     
     var body: some View {
         VStack {
-            HStack {
-                Button {
-                    presentationMode.wrappedValue.dismiss()
-                } label: {
-                    Text("Cancel")
-                }
-                
-                Spacer()
-                
-                Button {
-                    newTweetVM.uploadTweet(withBody: tweetBody)
-                } label: {
-                    Text("Tweet")
-                        .font(.subheadline)
-                        .bold()
-                        .padding(.horizontal, 12)
-                        .padding(.vertical, 6)
-                        .background(Color.twitterBlue)
-                        .foregroundColor(.white)
-                        .clipShape(Capsule())
-                }
-            }
-            .padding(.horizontal)
+            headerView
+            tweetEditorView
+            replyPersmissionsView
             
-            HStack(alignment: .top) {
-                // Placeholder blue circle
-                if authVM.currentUser?.profilePhotoUrl == nil {
-                    Circle()
-                        .frame(width: 32)
-                        .foregroundColor(.twitterBlue)
-                        .padding(.leading)
-                } else {
-                    // User profile photo, if it exists
-                    if let profilePhoto = authVM.currentUser?.profilePhotoUrl {
-                        KFImage(URL(string: profilePhoto))
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 32, height: 32)
-                            .clipShape(Circle())
-                            .padding(.bottom, 5)
-                            .padding(.leading)
-                            
-                    }
-                }
-                
-                
-
-                
-                VStack(alignment: .leading, spacing: 0) {
-                    HStack(spacing: 2) {
-                        Text("Public")
-                            .bold()
-
-                        Image(systemName: "chevron.down")
-                    }
-                    .font(.subheadline)
-
-                    TweetTextEditor(tweetBody: $tweetBody, placeholder: "What's happening?")
-                        .offset(x: -6, y: -6)
-                }
-            }
+            Divider()
+            
+            tweetExtrasView
+            
+        }
+        .onAppear() {
+            focusTweetBody = true
+        }
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker(image: $selectedImage)
         }
         // When didUploadTweet's change is published
         .onReceive(newTweetVM.$didUploadTweet) { success in
@@ -96,4 +53,154 @@ struct NewTweetView_Previews: PreviewProvider {
         NewTweetView()
             .environmentObject(AuthViewModel())
     }
+}
+
+extension NewTweetView {
+    // Cancel, Tweet buttons
+    var headerView: some View {
+        HStack {
+            Button {
+                presentationMode.wrappedValue.dismiss()
+            } label: {
+                Text("Cancel")
+                    .foregroundColor(.twitterBlue)
+            }
+            
+            Spacer()
+            
+            Button {
+                newTweetVM.uploadTweet(withBody: tweetBody)
+            } label: {
+                Text("Tweet")
+                    .font(.subheadline)
+                    .bold()
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 6)
+                    .background(Color.twitterBlue)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+            }
+        }
+        .padding(.horizontal)
+    }
+    // Profile photo, audience, tweet body
+    var tweetEditorView: some View {
+        HStack(alignment: .top) {
+            
+            // If no profile photo exists, display a placeholder circle
+            if authVM.currentUser?.profilePhotoUrl == nil {
+                Circle()
+                    .frame(width: 32)
+                    .foregroundColor(.twitterBlue)
+                    .padding(.leading)
+            } else { // If user photo exists
+                if let profilePhoto = authVM.currentUser?.profilePhotoUrl {
+                    KFImage(URL(string: profilePhoto))
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 32, height: 32)
+                        .clipShape(Circle())
+                        .padding(.bottom, 5)
+                        .padding(.leading)
+                    
+                }
+            }
+            
+            
+            
+            // Audience, tweet body
+            VStack(alignment: .leading, spacing: 0) {
+                
+                // Audience button
+                HStack(spacing: 2) {
+                    Text("Public")
+                        .bold()
+                    
+                    Image(systemName: "chevron.down")
+                }
+                .foregroundColor(.twitterBlue)
+                .padding(.vertical, 4)
+                .padding(.horizontal, 14)
+                .font(.subheadline)
+                .background {
+                    Capsule()
+                        .strokeBorder(style: StrokeStyle())
+                        .foregroundColor(.twitterBlue)
+                }
+                
+                
+                // Tweet body text editor
+                TextField("What's happening?", text: $tweetBody, axis: .vertical)
+                    .focused($focusTweetBody)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(selectedImage != nil ? 4 : 100)
+                    .padding(.vertical, 10)
+                    .padding(.trailing)
+                
+                // Tweet image
+                ScrollView {
+                    // If an image has been selected
+                    if let tweetImage = selectedImage {
+                        // Display tweet image
+                        Image(uiImage: tweetImage)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: .infinity)
+                            .padding(.leading, 0)
+                            .padding(.trailing)
+                        
+                    }
+                }
+                Spacer()
+            }
+            
+            
+            
+            
+            
+        }
+    }
+
+    // Reply persmissions
+    var replyPersmissionsView: some View {
+        HStack {
+            Image(systemName: "globe.asia.australia.fill")
+            Text("Everyone can reply")
+            Spacer()
+        }
+        .padding(.horizontal)
+        .font(.caption)
+        .foregroundColor(.twitterBlue)
+    }
+    
+    var tweetExtrasView: some View {
+        HStack(spacing: 20) {
+            Image(systemName: "mic.fill.badge.plus")
+                .foregroundColor(Color(red: 0.45, green: 0.3, blue: 0.9))
+            
+            Image(systemName: "waveform")
+            Button {
+                showingImagePicker = true
+            } label:{
+            Image(systemName: "photo")
+        }
+            
+            Image(systemName: "checklist")
+            
+            Image(systemName: "mappin.and.ellipse")
+            
+            Spacer()
+            
+            Image(systemName: "circle")
+            
+            Divider()
+                .frame(height: 36)
+                .padding(.vertical, 4)
+            
+            Image(systemName: "plus.circle.fill")
+        }
+        .foregroundColor(.twitterBlue)
+        .padding(.horizontal)
+    }
+    
 }
